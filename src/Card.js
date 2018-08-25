3a59cb4d985dc08d170021695d4239b5d7ecee53
Ola Fawol <ola.fawole5@gmail.com>
2018-08-25 17:07:20 +0100

    Improving performance

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({ id, name, email }) => {
 	//const { id, name, email } = props; // one way of destruction and the other passing it as a parameter in the above 
 	return (
 		<div className=' tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
-			<img alt='robot' src={`https://robohash.org/${id}?70x70`} />
+			<img alt='robot' src={`https://robohash.org/${id}?size=70x70`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
